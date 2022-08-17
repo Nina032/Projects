6630447dbe8ae81b13d6a6c85f459e6e8f1f1100
Nina032 <111436933+Nina032@users.noreply.github.com>
2022-08-17 17:54:30 +0200

    First exempel
    Co-Authored-By: Nina032 <111436933+nina032@users.noreply.github.com>

---
+++ ExempelProject/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ExempelProject
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+ 
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+           app.UseDefaultFiles();
+           app.UseStaticFiles();
+        }
+    }
+}
---
+++ ExempelProject/wwwroot/js/index.js
@@ -0,0 +1,6 @@
+﻿console.log("HelloWorld");
+function Namn() {
+
+}
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
